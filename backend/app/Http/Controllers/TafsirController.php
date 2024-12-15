292bd2aa45e92e5c0b4e98cfd5bca9e5aa73e0e1
arsyadkr <136600690+arsyadkr@users.noreply.github.com>
2024-12-15 11:33:02 +0700

    Tafsir Database
    Membuat tafsir database

---
+++ backend/app/Http/Controllers/TafsirController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tafsir;
+use App\Http\Resources\TafsirResource;
+use Illuminate\Support\Facades\Http;
+
+class TafsirController extends Controller
+{
+    public function index()
+    {
+        $client = new \GuzzleHttp\Client([
+            'timeout' => 7200,
+            'connect_timeout' => 7200,  
+            'retry' => 10,
+        ]);
+
+        for ($nomor = 1; $nomor <= 114; $nomor++) {
+            $url = "https://equran.id/api/v2/tafsir/{$nomor}"; 
+            $response = Http::get($url);
+
+            if ($response->successful()) {
+                $data = $response->json()['data']['tafsir']; 
+
+                foreach ($data as $ayat) {
+                    Tafsir::updateOrCreate(
+                        [
+                            'surat_nomor' => $nomor,         
+                            'nomor_ayat' => $ayat['ayat'],   
+                        ],
+                        [
+                            'teks_tafsir' => $ayat['teks'],
+                        ]
+                    );
+                }
+            } else {
+                return response()->json([
+                    'code' => 500,
+                    'message' => "Failed to fetch data for surah {$nomor} from external API"
+                ], 500);
+            }
+        }
+
+        return response()->json([
+            'code' => 200,
+            'message' => 'All tafsir data retrieved and stored successfully'
+        ]);
+    }
+
+    public function tafsir($nomor) {
+        $tafsir = Tafsir::where('surat_nomor', $nomor)->get();
+
+        return response()->json([
+            'code' => 200,
+            'message' => 'Data retrieved successfully',
+            'data' => TafsirResource::collection($tafsir),
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\SuratController;
 use App\Http\Controllers\AyatController;
+use App\Http\Controllers\TafsirController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/surat', [SuratController::class, 'index']);
 Route::get('/surat/all', [AyatController::class, 'index']);
 Route::get('/surat/{nomor}', [AyatController::class, 'ayat']);
+Route::get('/tafsir/all', [TafsirController::class, 'index']);
+Route::get('/tafsir/{nomor}', [TafsirController::class, 'tafsir']);
